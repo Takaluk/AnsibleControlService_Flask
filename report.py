473ecb72cae224bd559b6f6c65976d1653bc69c9
Takaluk <ingress041@gmail.com>
2024-10-26 12:46:08 +0000

    feat: graph for report

--- report.py
+++ report.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, send_file
 import subprocess
 import json
 import glob
 import os
+import pdfkit
 
 report_bp = Blueprint('report', __name__, url_prefix='/report')
 
@@ -14,7 +15,24 @@ def load_checklist_numbers(filepath):
 def load_server_status(filepath):
     with open(filepath) as json_file:
         return json.load(json_file)
-
+def read_json_file(file_path):
+    # sudo 권한으로 파일 읽기
+    command = f"sudo cat {file_path}"
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
+        return json.loads(result.stdout)
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return []
+def append_action_messages(scan_results, action_data):
+    for item in action_data:
+        number = item["number"]
+        msg = item["msg"]
+        if number in scan_results:
+            if isinstance(scan_results[number], list):
+                scan_results[number].append(msg)
+            else:
+                scan_results[number] = [scan_results[number], msg]
 @report_bp.route('/')
 def report():
     # 서버 이름 목록을 가져옴
@@ -64,6 +82,19 @@ def report():
     os_compliance = {}
     vulnerability_counts = {}
 
+    sudo_command = 'sudo cat /AnsibleVulnScanner/docs/linux_action_inform.json'
+    json_output = subprocess.getoutput(sudo_command)
+    try:
+        linux_action_data = json.loads(json_output)
+    except json.JSONDecodeError:
+        linux_action_data = []
+
+    sudo_command = 'sudo cat /AnsibleVulnScanner/docs/windows_action_inform.json'
+    json_output = subprocess.getoutput(sudo_command)
+    try:
+        windows_action_data = json.loads(json_output)
+    except json.JSONDecodeError:
+        windows_action_data = []
     for file in json_files:
         with open(file) as json_file:
             data = json.load(json_file)
@@ -76,7 +107,6 @@ def report():
                             server['scan_results'] = scan_results
                             department = server.get('department')
                             os_type = server.get('os')
-
                             # Calculate compliance ratio
                             total_checklist_items = original_windows_checklist_numbers if os_type == 'Windows' else original_linux_checklist_numbers
                             total_items_count = len(total_checklist_items)
@@ -84,19 +114,19 @@ def report():
                             compliant_count = sum(1 for value in scan_results.values() if value is True)
                             compliant_ratio = round((compliant_count / (total_items_count - non_compliant_count)) * 100, 2) if total_items_count > 0 else 0
                             server['compliance_ratio'] = compliant_ratio
-
+                            
                             # Update department compliance
                             if department:
                                 if department not in department_compliance:
                                     department_compliance[department] = []
                                 department_compliance[department].append(compliant_ratio)
-
+                            
                             # Update OS compliance
                             if os_type:
                                 if os_type not in os_compliance:
                                     os_compliance[os_type] = []
                                 os_compliance[os_type].append(compliant_ratio)
-
+                            
                             # Update vulnerability counts
                             for key, value in scan_results.items():
                                 if value is False:
@@ -104,14 +134,19 @@ def report():
                                         vulnerability_counts[key] = 0
                                     vulnerability_counts[key] += 1
                             break
-
     # Calculate average compliance for departments and OS types
     department_avg_compliance = {dept: round(sum(ratios) / len(ratios), 2) for dept, ratios in department_compliance.items()}
     os_avg_compliance = {os_type: round(sum(ratios) / len(ratios), 2) for os_type, ratios in os_compliance.items()}
 
+    for server in servers_list:
+        # Append action messages based on OS type
+        os_type = server.get('os')
+        if os_type == 'Windows':
+            append_action_messages(server['scan_results'], windows_action_data)
+        else:
+            append_action_messages(server['scan_results'], linux_action_data)
     # Get top 10 vulnerabilities
     top_vulnerabilities = sorted(vulnerability_counts.items(), key=lambda item: item[1], reverse=True)[:10]
-
     return render_template('report_template.html', servers=servers_list,
                            department_compliance=department_avg_compliance,
                            os_compliance=os_avg_compliance,
